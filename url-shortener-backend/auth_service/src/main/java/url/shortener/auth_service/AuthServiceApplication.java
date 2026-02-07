92585e3443b30a69548d45a892859f6d9492fb00
Habibur Rahman <habib.austcse.34@gmail.com>
2026-02-07 21:43:04 +0600

    Setup the auth-service

---
+++ url-shortener-backend/auth_service/src/main/java/url/shortener/auth_service/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package url.shortener.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+		System.out.println("Hello from Auth Service");
+	}
+}
